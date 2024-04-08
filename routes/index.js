626de5516a0e69a943fbb479aac37c7014740975
Hoang Anh <hoanganhlclqd@gmail.com>
2024-04-08 17:42:40 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs').promises;
 const path = require('path');
-const controller = require ("../public/controllers/DBController")
+const controller = require ("../controllers/DBController")
 
 router.get('/', controller.getAllAdRates);
 
